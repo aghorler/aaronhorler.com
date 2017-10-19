9ac25e12d06387fb9a53677adfce813583fd914f
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-19 22:31:59 +1100

    Minified JavaScript.

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,22 @@
 var pluto = document.getElementById("profile");
 
 function spinPluto(){
-	if(pluto.style.transform == "" || pluto.style.transform == "rotateZ(-360deg)"){
-		for(let i=0; i<361; i++){
-			setTimeout(function timer(){
-				pluto.style.transform = "rotateZ(" + i + "deg)";
-			}, i*4);
-		}
-	}
-	else if(pluto.style.transform == "rotateZ(360deg)"){
-		pluto.style.transform = "rotateZ(" + 0 + "deg)";
-		for(let i=0; i>-361; i--){
-			setTimeout(function timer(){
-				pluto.style.transform = "rotateZ(" + i + "deg)";
-			}, (i*-1)*4);
-		}
-	}
+  if(pluto.style.transform == "" || pluto.style.transform == "rotateZ(-360deg)"){
+    for(let i = 0; i < 361; i++){
+      setTimeout(function timer(){
+        pluto.style.transform = "rotateZ(" + i + "deg)";
+      }, i*4);
+    }
+  }
+  else if(pluto.style.transform == "rotateZ(360deg)"){
+    pluto.style.transform = "rotateZ(" + 0 + "deg)";
+
+    for(let i = 0; i > -361; i--){
+      setTimeout(function timer(){
+        pluto.style.transform = "rotateZ(" + i + "deg)";
+      }, (i*-1)*4);
+    }
+  }
 }
 
 function encryptMessage(){

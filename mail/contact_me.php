ba47f20908ad079035339de7c1b74412fb4c09bc
Aaron Horler <aghorler@users.noreply.github.com>
2018-01-05 09:02:30 +1100

    Alter public email address

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,12 +12,9 @@ if(empty($_POST['name'])  		||
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
-// Include to email address.
-include('email.php');
 	
 // Create the email and send the message
-$to = 'aghorler@gmail.com';
+$to = 'hello@aaronhorler.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message\n\nIP address: " . $_SERVER['REMOTE_ADDR'];
 $headers = "From: noreply@aaronhorler.com\n";
